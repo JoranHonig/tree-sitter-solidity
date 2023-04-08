ece6257f84b04b2d7ab092c1a4a47a1da90e055a
Michal Převrátil <michprev@gmail.com>
2023-04-08 14:51:52 +0200

    🔖 Version 1.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with Path(__file__).parent.joinpath("README.md").open() as f:
 
 setup(
     name="abch_tree_sitter_solidity",
-    version="0.0.1",
+    version="1.0.0",
     author="Ackee Blockchain",
     url="https://github.com/Ackee-Blockchain/tree-sitter-solidity",
     license="MIT",
